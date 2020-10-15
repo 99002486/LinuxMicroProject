0ecbf8d1771055ff8706af0db8705d6d108823f1
99002486 <71490592+99002486@users.noreply.github.com>
2020-10-15 10:27:43 +0530

    Update microProject.sh

--- microProject.sh
+++ microProject.sh
@@ -13,7 +13,6 @@ while IFS=, read -r NAME EMAIL REPO; do
         [[ $? == 0 ]] && printf "Clone Success," >> $RESULT_FILE
         [[ $? > 0 ]] && printf "Clone failed," >> $RESULT_FILE
         REPO=`echo "$REPO" | cut -d'/' -f5`
-        echo "REPO = $REPO"
         DIR=`find "$REPO" -name "Makefile" -exec dirname {} \;`
         make -C "$DIR"
         [[ $? == 0 ]] && printf "build Success," >> $RESULT_FILE
@@ -22,7 +21,6 @@ while IFS=, read -r NAME EMAIL REPO; do
         printf "$ERRORS," >> $RESULT_FILE
         make test -C "$DIR"
         EXECUTABLE=`find "$DIR" -name "Test*.out"`
-        echo "$EXECUTABLE"
         valgrind "./$EXECUTABLE" 2> valgrinderr.csv
         VALGRINDERR=`grep "ERROR SUMMARY" valgrinderr.csv`
         printf "${VALGRINDERR:24:1} \n" >> $RESULT_FILE
